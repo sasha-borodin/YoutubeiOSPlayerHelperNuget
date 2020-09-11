c9a581f0563c8558949569c4a4bec492c6f8cc8d
chotaling1 <43324368+chotaling1@users.noreply.github.com>
2020-09-11 11:40:58 -0500

    Initial Commit

---
+++ Structs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ObjCRuntime;
+
+namespace YouTubeiOSPlayerHelper
+{
+
+	public enum YTPlayerState : int
+	{
+		Unstarted,
+		Ended,
+		Playing,
+		Paused,
+		Buffering,
+		Cued,
+		Unknown
+	}
+
+	public enum YTPlaybackQuality : int
+	{
+		Small,
+		Medium,
+		Large,
+		Hd720,
+		Hd1080,
+		HighRes,
+		Auto,
+		Default,
+		Unknown
+	}
+
+	public enum YTPlayerError : int
+	{
+		InvalidParam,
+		HTML5Error,
+		VideoNotFound,
+		NotEmbeddable,
+		Unknown
+	}
+}
+
